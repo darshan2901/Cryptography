076c02e0934a3b1cb12983d4743a028aa14eae41
Darshan Patel <60173648+darshan2901@users.noreply.github.com>
2020-12-31 11:12:01 +0530

    Caesar  cipher  encryption

---
+++ caesar1.py
@@ -0,0 +1,21 @@
+def encripted(string,shift):
+    ciper=''
+    for char in string:
+        if  char.isupper():
+            ciper=ciper+chr((ord(char)+shift-65)%26+65)
+
+    return ciper
+
+text=input("enter the text")
+s=int(input("enter the shift key"))
+print("the encripted string :",encripted(text,s))
+
+def decriptrd(string,shift):
+    ciper=''
+    for char in string:
+            if  char.isupper():
+                ciper=ciper+chr((ord(char)-shift-65)%26+65)
+    return ciper
+    text=input("enter the text")
+    s=int(input("enter the shift key"))
+    print("the dcripted string :",decriptrd(text,s))
